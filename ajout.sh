8dc89c67e8230c478f7c366703eaae4b314c54e4
PClero <152182239+PClero@users.noreply.github.com>
2023-12-15 11:21:27 +0100

    Update ajout.sh

--- ajout.sh
+++ ajout.sh
@@ -2,13 +2,12 @@
 folder=liste-sauvegarde
 while true:
 do
-# read -p "Veuillez entrer le chemin du fichier que vous souhaitez sauvegarder : " folder
 ajout_ligne() {
     echo "Entrez le chemin du répertoire à ajouter ou x pour sortir"
     read $chemin_repertoire  
     if
     [ -d $chemin_repertoire ]
-# grep -q "$chemin_repertoire" "$folder"; then
+
         echo "/$chemin_repertoire/d" >> "$folder"
         echo "Répertoire ajouté avec succès."
        else
