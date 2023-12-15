f23aba58e11054f2ab7dc5ac0a737b93edc3874d
PClero <152182239+PClero@users.noreply.github.com>
2023-12-15 11:12:43 +0100

    Update ajout.sh

--- ajout.sh
+++ ajout.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 folder=liste-sauvegarde
-while
-read -p "Veuillez entrer le chemin du fichier que vous souhaitez sauvegarder : " folder
+while true:
+do
+# read -p "Veuillez entrer le chemin du fichier que vous souhaitez sauvegarder : " folder
 ajout_ligne() {
-    echo "Entrez le chemin du répertoire à supprimer :"
+    echo "Entrez le chemin du répertoire à ajouter : "
     read $chemin_repertoire  
-    if 
-    grep -q "$chemin_repertoire" "$folder"; then
-        echo "/$chemin_repertoire/d" > "$folder"
-        echo "Répertoire supprimé avec succès."
+    if
+    [ -d $chemin_repertoire ]
+# grep -q "$chemin_repertoire" "$folder"; then
+        echo "/$chemin_repertoire/d" >> "$folder"
+        echo "Répertoire ajouté avec succès."
+    elif
+    
     else
         echo "Le répertoire n'est pas dans la liste."
-    fi
+    
 }
+done
