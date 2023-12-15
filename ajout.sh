ac4aee35dc3c83d38fd7cb6d8c69639e87b6401d
PClero <152182239+PClero@users.noreply.github.com>
2023-12-15 12:24:11 +0100

    Update ajout.sh

--- ajout.sh
+++ ajout.sh
@@ -1,22 +1,20 @@
-#!/bin/bash
-folder=liste-sauvegarde
-sortie=x
-while true
-do
-ajout_ligne() {
-    echo "Entrez le chemin du répertoire à ajouter ou x pour sortir"
-    read $chemin_repertoire  
-    if
-    [ -d $chemin_repertoire ]; then
-  
-        echo "/$chemin_repertoire/d" >> "$folder"
-        echo "Répertoire ajouté avec succès."
-       else
-        echo "Le répertoire n'est pas dans la liste."
-       
-      [ $sortie -eq x ]; then
-        break
-    fi
-    
-}
+folder="liste-sauvegarde" 
+
+sortie="x"
+while true; do
+    ajout_ligne() {
+
+        read -p "Entrez le chemin du répertoire à ajouter ou 'x' pour sortir : "      
+
+        if [ "$chemin_repertoire" = "x" ]; then
+            break
+        elif [ -d "$chemin_repertoire" ]; then
+            echo "/$chemin_repertoire/d" >> "$folder"
+            echo "Répertoire ajouté avec succès."
+        else
+            echo "Le répertoire n'est pas valide."  
+
+        fi
+    }
+    ajout_ligne
 done
