387595d9c0a5bccb4c1dd461cb12caf95fb87892
PClero <152182239+PClero@users.noreply.github.com>
2023-12-15 15:01:49 +0100

    Update sauver.sh

--- sauver.sh
+++ sauver.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 Sauvegarde=$1
+mkdir $Sauvegarde
 for repertoire in $( cat liste-sauvegarde ) 
 do
     if [ -e $repertoire ]; then 
             cp -R $repertoire $Sauvegarde
     fi
-done >> liste-sauvegarde
+done 
 
-mkdir $Sauvegarde
+
