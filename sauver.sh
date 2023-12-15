e08e6e0c9326a8c87fb4b6e956c9ab161b54cfec
AntoineFONTANILLE <152191628+AntoineFONTANILLE@users.noreply.github.com>
2023-12-15 11:30:58 +0100

    Update sauver.sh

--- sauver.sh
+++ sauver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 Sauvegarde=$1
-for repertoire in $( cat "/home/liste-sauvegarde" ) 
+for repertoire in $( cat liste-sauvegarde ) 
 do
     if [ -e $repertoire ]; then 
             cp -R $repertoire in $Sauvegarde
