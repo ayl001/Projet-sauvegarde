7690ae773f45b1db85f64a3b710a659f1bf50f23
PClero <152182239+PClero@users.noreply.github.com>
2023-12-19 16:34:15 +0100

    Update ajout.sh

--- ajout.sh
+++ ajout.sh
@@ -1,20 +1,79 @@
-folder="liste-sauvegarde" 
+#!/bin/bash
 
-sortie="x"
+
+
+# Fichier contenant la liste des répertoires
+
+fichier_liste="liste-sauvegarde"
+
+
+
+# Fonction pour ajouter un nouveau répertoire à la liste
+
+ajouter_repertoire() {
+
+    # Demande à l'utilisateur de saisir le chemin du répertoire
+
+    read -p "Entrez le chemin complet du nouveau répertoire : " nouveau_repertoire
+
+
+
+# Vérifie si le répertoire existe
+
+    if [ -d "$nouveau_repertoire" ]; then
+
+        # Ajoute le chemin du nouveau répertoire à la liste
+
+        echo "$nouveau_repertoire" >> "$fichier_liste"
+
+        echo "Chemin ajouté avec succès."
+
+    else
+
+        echo "Le répertoire $nouveau_repertoire n'existe pas."
+
+    fi
+
+}
+
+
+
+# Menu simple
+
 while true; do
-    ajout_ligne() {
 
-        read -p "Entrez le chemin du répertoire à ajouter ou 'x' pour sortir : "  chemin_repertoire
+    echo "1. Ajouter un nouveau répertoire"
 
-        if [ "$chemin_repertoire" = "x" ]; then
-            break
-        elif [ -d "$chemin_repertoire" ]; then
-            echo "/$chemin_repertoire/d" >> "$folder"
-            echo "Répertoire ajouté avec succès."
-        else
-            echo "Le répertoire n'est pas valide."  
+    echo "2. Quitter"
 
-        fi
-    }
-    ajout_ligne
+    
+
+    read -p "Choisissez une option (1 ou 2) : " choix
+
+
+
+    case $choix in
+
+        1)
+
+            ajouter_repertoire
+
+            ;;
+
+        2)
+
+            echo "Au revoir!"
+
+            exit 0
+
+            ;;
+
+        *)
+
+            echo "Option invalide. Veuillez choisir 1 ou 2."
+
+            ;;
+
+    esac
+
 done
