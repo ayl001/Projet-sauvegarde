5b47e1afa778e90e2ce16949c0f47c09f4a36f10
PClero <152182239+PClero@users.noreply.github.com>
2023-12-19 16:36:38 +0100

    Update ajout.sh

--- ajout.sh
+++ ajout.sh
@@ -1,79 +1,40 @@
 #!/bin/bash
 
-
-
 # Fichier contenant la liste des répertoires
-
 fichier_liste="liste-sauvegarde"
 
-
-
 # Fonction pour ajouter un nouveau répertoire à la liste
-
 ajouter_repertoire() {
-
     # Demande à l'utilisateur de saisir le chemin du répertoire
-
     read -p "Entrez le chemin complet du nouveau répertoire : " nouveau_repertoire
 
-
-
 # Vérifie si le répertoire existe
-
     if [ -d "$nouveau_repertoire" ]; then
-
         # Ajoute le chemin du nouveau répertoire à la liste
-
         echo "$nouveau_repertoire" >> "$fichier_liste"
-
         echo "Chemin ajouté avec succès."
-
     else
-
         echo "Le répertoire $nouveau_repertoire n'existe pas."
-
     fi
-
 }
 
-
-
 # Menu simple
-
 while true; do
-
     echo "1. Ajouter un nouveau répertoire"
-
-    echo "2. Quitter"
-
-    
+    echo "2. Quitter"    
 
     read -p "Choisissez une option (1 ou 2) : " choix
 
-
-
     case $choix in
-
         1)
-
             ajouter_repertoire
-
             ;;
-
         2)
-
             echo "Au revoir!"
-
             exit 0
-
             ;;
-
         *)
-
             echo "Option invalide. Veuillez choisir 1 ou 2."
-
             ;;
-
     esac
-
 done
