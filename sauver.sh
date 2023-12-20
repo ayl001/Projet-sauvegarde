05d8fe4cbc7e6fe253005b918a72bcdbc02804de
AntoineFONTANILLE <152191628+AntoineFONTANILLE@users.noreply.github.com>
2023-12-20 14:56:49 +0100

    Update sauver.sh

--- sauver.sh
+++ sauver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-Sauvegarde=${1}_$(date "+%Y_%m_%d_%H_%M") 
+Sauvegarde=$1_$(date "+%Y_%m_%d_%H_%M") 
 #déclaration de la variable avec la date
 mkdir -p $Sauvegarde 
 #création du dossier si celui ci n'existe pas déjà
 for repertoire in $( cat liste-sauvegarde ) #
 do
     if [ -e $repertoire ]; then 
-            cp -R $repertoire $Sauvegarde && echo "sauvegarde effectuée"
+            cp -R $repertoire $Sauvegarde
     fi
 done 
 
