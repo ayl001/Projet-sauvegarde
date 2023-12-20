1b15de96acdd281766dd2394f4bc55a627bbfa02
AntoineFONTANILLE <152191628+AntoineFONTANILLE@users.noreply.github.com>
2023-12-20 14:22:11 +0100

    Update sauver.sh

--- sauver.sh
+++ sauver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-Sauvegarde=$1
-mkdir $Sauvegarde
+Sauvegarde=${1}_$(date "+%Y_%m_%d_%H_%M") 
+bash ./sauver.sh $Sauvegarde ; read -p "..." ; clear
+mkdir -p $Sauvegarde
 for repertoire in $( cat liste-sauvegarde ) 
 do
     if [ -e $repertoire ]; then 
